239957b0b7673e90678db30a3f51a935c744d94c
Agam main <work.agamchopra@gmail.com>
2022-01-20 17:58:19 -0500

    grav sim added and bug fixes

--- grav.py
+++ grav.py
@@ -1,19 +1,17 @@
 import pygame
-from numpy import concatenate, nan_to_num, where, zeros, sum
+from numpy import concatenate, nan_to_num, where, zeros, sum, dot, ones
 from numpy.random import randint
 from scipy.spatial.distance import cdist
 from numba import jit
 
-SIG = 2.6E-3
-EPS = 1E-4
-MASS = 4.65E-26
+G = 6.67430E-11
 FPS = 144
 SPF = 1/FPS
 RADIUS = 3
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 900, 900
 DISH = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('sim')
 
@@ -21,16 +19,16 @@ EPSILON = 1E-20
 
 
 @jit(nopython=False)
-def Lennard_Jones_dynamics(ringo, color, RADIUS=1E-6, MASS=1E-6, SPF=1/144, WIDTH=600, HEIGHT=600, SIG=SIG, EPS=EPS):
+def newtonian_gravitational_dynamics(ringo, color, M, SPF=1/144, WIDTH=600, HEIGHT=600):
     x = ringo[:, :2]
     R = cdist(x, x, metric='euclidean')
     R_ = zeros((x.shape[0], x.shape[0], x.shape[1]))
+    M_ = dot(M,M.T)
 
     for i in range(x.shape[0]):
         R_[i] = (x - x[i]) / (R[i].reshape(R.shape[0], 1) + EPSILON)
-
-    a = sum(nan_to_num((48/MASS) * (EPS/SIG) * ((SIG/(R + EPSILON)) ** 13 -
-            (((SIG/(R + EPSILON)) ** 7) * 0.5))).reshape(R.shape[0], R.shape[0], 1) * R_, axis=1)
+    
+    a = sum(nan_to_num((1/(M * ones((R.shape[0], R.shape[0])))) * G * (M_/((R ** 2) + EPSILON))).reshape(R.shape[0], R.shape[0], 1) * R_, axis=1)
     v = ringo[:, 2:] + (a * SPF)
     x = x + (v * SPF)
     
@@ -55,11 +53,17 @@ def draw_window(tensor, lighting):
 def main():
     clock = pygame.time.Clock()
     run = True
-    N = 300
+    N = 9
+    M = ones((N, 1))*1E9
+    M[0] = M[0]*1E6
     width = randint(0, WIDTH, (N, 1)).astype('float64')
+    width[0] = WIDTH/2
     height = randint(0, HEIGHT, (N, 1)).astype('float64')
-    vx = randint(-50, 50, (N, 1)).astype('float64')*0.
-    vy = randint(-50, 50, (N, 1)).astype('float64')*0.
+    height[0] = HEIGHT/2
+    vx = randint(-50, 50, (N, 1)).astype('float64')*0.4
+    vy = randint(-50, 50, (N, 1)).astype('float64')*0.4
+    vx[0] = 0.
+    vy[0] = 0.
     ringo = concatenate((width, height, vx, vy), axis=1)
     color = randint(150, 255, (N, 3))
     time_step = 0
@@ -70,8 +74,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        arty, ringo = Lennard_Jones_dynamics(
-            ringo, color, RADIUS=RADIUS*1E-6, MASS=MASS, WIDTH=WIDTH, HEIGHT=HEIGHT, SPF=SPF, EPS=EPS)
+        arty, ringo = newtonian_gravitational_dynamics(ringo, color, M=M, WIDTH=WIDTH, HEIGHT=HEIGHT, SPF=SPF)
         draw_window(arty, lighting)
         time_step += 1
         # print(ringo[0])
